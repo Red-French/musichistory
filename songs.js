55385283c21fc89718423670bafffa584d980014
Red French <french@volstate.net>
2015-10-21 14:20:36 -0500

    added ability for user to add songs to list

--- songs.js
+++ songs.js
@@ -1,7 +1,8 @@
 var songs = [];
-var badCharPosition;
+// var badCharPosition;
 var songlist = document.getElementById("results");
-var songsToTextArea;
+var results;
+
 
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
@@ -9,15 +10,16 @@ songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album Th
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-songArray = songs;
+// songArray = songs;
 
 songs.unshift("Hey, Big Programmer > by Data Man on the album Git Hell");
 songs.push("Old Five and Dimers > by Jug Head on the album Big Thugs");
 
 // console.log(songArray);
-
+addSong();
+function addSong(newSong) {
 for (var i = 0; i < songs.length; i++) {
-	console.log(songs[i]);
+	// console.log(songs[i]);
 	// console.log("length of entry = ", songs[i].length);
 
 	// badCharPosition = songs[i].indexOf(">"); // locates position of ">"
@@ -29,13 +31,52 @@ for (var i = 0; i < songs.length; i++) {
 
 // songz = songArray.join("\n");
 // replace(",", "<br>");
-songlist.innerHTML += "<blockquote>" + songs[i] + "</blockquote>"; // only shows last song
+// songlist.innerHTML += "<blockquote>" + results + "</blockquote>";
+
+songlist.innerHTML += "<blockquote>" + songs[i] + "</blockquote>";
+
 	// songlist.innerHTML = songArray;
 // songlist.innerHTML = songArray; // shows all songs with comma separation -- NOT 1 per line
 // songlist.innerHTML = document.write(songs[i]);
 // songlist.innerHTML = document.write("<br>");
 
 }
-console.log(songArray);
+// console.log(songArray);
+}
+
+// 	'ADD' BUTTON (IN NAV) EVENT LISTENER DISPLAYS FORM AND HIDES SONGLIST
+document.getElementById("add").addEventListener("click", function() {
+	console.log("add link clicked");
+	songlist.innerHTML = "<p>Song: <input id='newSong' class='inputButton' type='text' value=''></p>" + "<p>Artist: <input id='newArtist' class='inputButton' type='text' value=''></p>" + "<p>Album: <input id='newAlbum' class='inputButton' type='text' value=''></p>" + "<p><input class='addToSongList' type='button' value='Add Song to List'></p>";
+});
+
+// 'LIST' BUTTON (IN NAV) EVENT LISTENER DISPLAYS SONGLIST AND HIDES FORM
+document.getElementById("list").addEventListener("click", function() {
+	console.log("list link clicked");
+	songlist.innerHTML = "";
+	for (var i = 0; i < songs.length; i++) {
+		songlist.innerHTML += "<blockquote>" + songs[i] + "</blockquote>";
+	}
+});
+
+// 'ADD SONG TO LIST' BUTTON (DYNAMICALLY PRODUCED BY 'ADD' BUTTON (IN NAV)
+// ADDS SONG TO SONG ARRAY
+document.querySelector("body").addEventListener("click", function(event) {
+  console.log(event);
+
+  if (event.target.className === "addToSongList") {
+    console.log("add song to list clicked");
+    newSong = document.getElementById("newSong");
+    newSong = newSong.value;
+    newArtist = newArtist.value;
+    newAlbum = newAlbum.value;
+    songs.push(newSong + " by " + newArtist + " on the album " + newAlbum);
+    console.log(songs);
+    songlist.innerHTML = "";
+    addSong(newSong);
+  }
+});
+
+
 
 
