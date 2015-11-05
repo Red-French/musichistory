810598de439f9107908b29d16aaaca72c4abdd6c
Red-French <red@redfrench.net>
2015-11-05 00:27:12 -0600

    bootstrapping/sass changeover - songs are populating - needs 'add music' link to work

---
+++ javascripts/get-more-songs.js
@@ -0,0 +1,39 @@
+define(["jquery"],
+  function($) {
+  console.log("inside populate-songs");
+  return {
+    getMeSomeData: function(callbackFunctionReference) {
+      console.log("inside function");
+      $.ajax({url: "javascripts/more-songs.json",
+      }).done(function(firstSongsReturned) {
+        callbackFunctionReference(firstSongsReturned);
+      })  
+    }
+  }
+})
+
+
+// define( [], function() {
+// console.log("inside get-more-songs.js");
+
+//     $.ajax({
+//       url: "javascripts/more-songs.json",
+//     }).done(function(moreSongs) {
+//       addSong(songArray);
+//     });
+
+
+// // var songlist = document.getElementById("results");
+//   function addSong(songArray) {
+//     console.log("in addSong function");
+//     console.log(songArray);
+//     var results = $("#results");
+//     // for (var i = 0; i < songArray.length; i++) {
+//     songArray.songs.forEach(function(song){
+//     var string = "<div>" + song.title + " " + song.artist + " " + song.album + "<input class='clear-message' type='button' value='Delete'>" + "</div>";
+//     results.append(string);
+//       });
+//         $("#results").after("<button type='button' id='moreButton'>More</button>");
+//   }
+// });
+
---
+++ javascripts/populate-songs.js
@@ -0,0 +1,113 @@
+
+// define(["jquery"], function($) {
+//   return {
+//     getMeSomeData: function(callbackFunctionReference) {
+//       $.ajax({...}).done(function(jsonDataSentByjQuery) {
+//         callbackFunctionReference(jsonDataSentByjQuery);
+//       }
+//     }
+//   }
+// });
+
+define(["jquery"],
+  function($) {
+  console.log("inside populate-songs");
+  return {
+    getMeSomeData: function(callbackFunctionReference) {
+      console.log("inside function");
+      $.ajax({url: "javascripts/songArray.json",
+      }).done(function(firstSongsReturned) {
+        callbackFunctionReference(firstSongsReturned);
+      })  
+    }
+  }
+})
+
+
+//////////Luke Wrote This////////////////
+// function add(a, b) {
+//   returns a + b;
+// }
+
+// add(1, 2);
+
+// function math(a, b, callbackFunction) {
+//   callbackFunction(a, b);
+// }
+
+// math(1, 2, add);
+///////^^^Luke Wrote This^^^/////////////
+
+
+
+// function callme (numA, callback) {
+//   var newNum = numA * 10;
+//   callback(newNum);
+// }
+
+// function later (numB) {
+//   console.log("final", numB * 8);
+// }
+
+// callme(5, later);
+
+
+// });
+
+    // define( [], function() {
+    // console.log("inside populate-songs.js");
+    //   $.ajax({
+    //       url: "javascripts/songArray.json",
+    //     }).done(function(songArray) {
+    //       addSong(songArray);
+    //       // console.log("the contents of songArray:");
+    //       // console.log(songArray);
+    //     });
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////
+
+
+
+
+// // JACOB'S FUNCTION (forEach) -- THANK YOU!
+// function printString(songsObj){
+//  var songbox = $("#songbox");
+//    songsObj.songs.forEach(function(song){
+//      var string = "<div>" + songs.title + " " + songs.artist + " " + songs.album + "</div>";
+//        songbox.append(string);
+//      });
+// }
+
+
+//   // DELETE INDIVIDUAL SONGS
+//   document.querySelector("body").addEventListener("click", function(event) {
+//     // console.log(event);
+//     if (event.target.className === "clear-message") {
+//       // console.log("You clicked on `Delete Message`");
+//       // console.log(event.target.parentElement);
+//       // console.log("events:", event);
+//       // event.target.parentElement.innerHTML = "";
+//       event.target.parentElement.setAttribute("hidden", true);
+//     }
+//     // if (messageBoard.innerHTML.length < 1) {     // WORK ON THIS
+//     //  clearAll.disabled = true;
+//     // }
+//     $("#moreButton").click(function() {
+//       $.ajax({
+//       url: "get-more-songs.js"
+//     }).done(addSong);
+//     });
+//   });
+
+
+//   return {
+//     printString: printString
+//   };
+// });
+
+
+
