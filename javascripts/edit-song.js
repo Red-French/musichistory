ac67721d44f4c52434d3c67e8c2a6a358bcf0a23
Red-French <red@redfrench.net>
2015-12-02 14:37:18 -0600

    working on change to edit song info

---
+++ javascripts/edit-song.js
@@ -0,0 +1,12 @@
+define(["jquery", "q", "populate-songs", "refresh"], 
+  function($, Q, populate_songs, refresh){
+
+
+on click of edit button
+grab id of button
+grab database song info using id (above)
+bring up 'add song' form pre-populated with song info to be edited
+click of add should call add_songs.js
+
+});
+
