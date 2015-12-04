d3ba9d8319a026eb60845cb03075c746b2e54356
vagrant <red@redfrench.net>
2015-12-04 09:57:12 -0600

    titles populating & search box working

--- app/controllers/songCtrl.js
+++ app/controllers/songCtrl.js
@@ -1,18 +1,20 @@
-app.controller("SongCtrl", 
-  ["$q", "$http", "$scope", "songFactory", 
+// the controller is the brain
+
+app.controller("songCtrl", // 'songCtrl' is name given to this file/contoller
+  ["$q", "$http", "$scope", "songFactory", // 'songFactory' is name given to 'songStorage.js'
   function($q, $http, $scope, songFactory) {
 
   $scope.searchText = "";
-  $scope.songs = [];
+  $scope.songs = [];  // place 'songs' (new array) on 'scope' object
 
-  songFactory.loadSongs().then {
+  songFactory.loadSongs().then(  // execute loadSongs() in 'songFactory' (songStorage.js) - 'then' is a listener
     function() {
-      scope.songs = songFactory.getSongs();
+      $scope.songs = songFactory.getSongs();  // execute getSongs() in 'songFactory' (songStorage.js)
     },
     function (error) {
       console.log(error);
     }
-  };
+  );
 
 
     $scope.newSong = { artist: "", album: "", name: ""};
--- app/controllers/songFormCtrl.js
+++ app/controllers/songFormCtrl.js
@@ -1,18 +1,5 @@
-var app = angular.module("loadSongsApp", ["ngRoute"]);
-
-app.controller("songLoadCtrl", ["$q", "$http", "$scope" "simple-songs", 
-function($q, $http, $scope, simpleSongs) {
-
-app.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/songs/list', {
-        templateUrl: 'partials/song-list.html',
-        controller: 'SongCtrl'
-      }).
-      when('/songs/add', {
-	    templateUrl: 'partials/song-form.html',
-	    controller: 'SongFormCtrl'
-      })
-      .otherwise('songs/list');  // catchall
-  }]);
+app.controller("songFormCtrl", ["$q", "$http", "$scope", "songFactory", 
+function($q, $http, $scope, songFactory) {
+ // $scope.cat = "cat";
+ // THIS FILE DOES WHAT?
+}]);
