456019bd04333cdbaecb5cc17f51d1fd0406afe7
Red-French <red@redfrench.net>
2015-12-04 09:57:12 -0600

    titles populating & search box working

--- app/app3.js
+++ app/app3.js
@@ -1,15 +1,17 @@
-var app = angular.module("MusicApp", ["ngRoute"]);
+var app = angular.module("MusicApp", ["ngRoute"]);  // this is like require
+  // global 'angular' object has a function, 'module', that takes a name ('MusicApp') and
+  // also takes an array of dependencies.
 
-app.config(['$routeProvider',
- function($routeProvider) {
+app.config(['$routeProvider',  // this is like entry.js
+ function($routeProvider) {  // 'routeProvider' is the traffic director
    $routeProvider
-   .when('/songs/list', {
-       templateUrl: 'partials/songList.html',
-       controller: 'MusicCtrl'
+   .when('/songs/list', {  // when URL ends with this
+       templateUrl: '../partials/songList.html',  // location of template
+       controller: 'songCtrl'  // name of controller (songCtrl.js)
      })
-     .when('/songs/form', {
-       templateUrl: 'partials/songForm.html',
-       controller: 'MusicCtrl'
+     .when('/songs/form', {  // when URL ends with this
+       templateUrl: '../partials/songForm.html',  //  location of template
+       controller: 'songFormCtrl'  //  name of controller (songFormCtrl.js)
      })
      .otherwise('/songs/list'); // catchall
  }]);
--- app/factories/songStorage.js
+++ app/factories/songStorage.js
@@ -1,5 +1,8 @@
-app.factory("songFactory", 
+app.factory("songFactory", // 'songFactory' is name given to this file/factory
 ["$q", "$http", 
+
+// PRIVATE
+
 function($q, $http) {
 
   var song_list;
@@ -11,41 +14,45 @@ function($q, $http) {
   // ];
 
   function loadSongs () {
-    return $q(function(resolve, reject) {
-      $http.get('../songArray.json')
-      .success(
-        function(objectFromJSONFile) {
+    return $q(function(resolve, reject) {  // promises
+      $http.get('../../songArray.json')  // $http is the Angular AJAX call
+      .success(  // promises uses '.success' for success or failure of the call
+        function(objectFromJSONFile) { 
           /* 
           Convert Firebase's object of objects into an array of objects, and store it in the private variable
           */
-          song_list = Object.keys(objectFromJSONFile.songs).map(song => objectFromJSONFile.songs[song]);
-          resolve(objectFromJSONFile.songs);
-        }, function(error) {
+          // song_list = Object.keys(objectFromJSONFile.songs).map(song => objectFromJSONFile.songs[song]);
+          song_list = objectFromJSONFile.songs;  // move array of objects into variable
+          // console.log("songFactory>loadSongs()>song_list", objectFromJSONFile.songs);
+          resolve(objectFromJSONFile.songs);  // if call is successful
+        }, function(error) {  // if call fails
           reject(error);
         }
       );
     });
   }
 
-  // Store the promise as a private variable
-  var songPromise = loadSongs();
+  var songPromise = loadSongs();  // Store the promise as a private variable
+
+// PUBLIC
 
   return {
     loadSongs: function() {
       return songPromise;
-    }
+    },
     getSongs: function() {
-      console.log("Factory returning all songs");
+      console.log("Factory returning song_list", song_list);
       return song_list;
     },
     getSong: function(name) {
       console.log("Factory returning single song");
       return song_list.filter(function(song){
-        return song.name === name;
+        return songs.name === name;
       })[0];
     },
     addSongs: function(songs) {
-      song_list = Object.keys(songs).map(s => songs[s]);
+      // song_list = Object.keys(songs).map(s => songs[s]);
+      song_list = objectFromJSONFile.songs;  // move array of objects into variable
       console.log("Song list added to factory");
       // song_list = songs;
       return song_list;
@@ -56,4 +63,5 @@ function($q, $http) {
       return song_list;
     }
   }
-});
+}
+]);
