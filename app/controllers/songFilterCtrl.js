2ffe523b3b6e982befcf51699a81ffba90bfd438
Red-French <red@redfrench.net>
2016-03-30 17:51:44 -0500

    work on filtering - not working

--- app/controllers/songFilterCtrl.js
+++ app/controllers/songFilterCtrl.js
@@ -1,10 +1,9 @@
 app.controller("songFilterCtrl",
   ["$scope", "$routeParams", "$firebaseArray",
-  function($scope, $routeParams, $firebaseArray, songId) {
-    console.log("songId", songId);
+  function($scope, $routeParams, $firebaseArray) {
     $scope.selectedSong = {};
-    // $routeParams allows you to grab the value of the current route parameters.  
-    // So, here we can pluck out the id of the particular song,  
+    // $routeParams allows you to grab the value of the current route parameters.
+    // So, here we can pluck out the id of the particular song,
     // because we set it in the href of the link from song-list partial.
     $scope.songId = $routeParams.songId;
 
@@ -14,23 +13,23 @@ app.controller("songFilterCtrl",
   // Make sure you use the $loaded promise handler, which waits
   // for all songs to be loaded from the reference before you try
   // to grab the record the user wanted.
-    $scope.songs.$loaded()
-      .then(function() {
-        console.log("scope.songs", $scope.songs)
-      // The $getRecord method on a $firebaseArray is very useful
-        $scope.selectedSong = $scope.songs.$getRecord($scope.songId);
-        console.log("$scope.selectedSong", $scope.selectedSong);
-      })
-      .catch(function(error) {
-        console.log("Error:", error);
-      });
+    // $scope.songs.$loaded()
+    //   .then(function() {
+    //     console.log("scope.songs", $scope.songs)
+    //   // The $getRecord method on a $firebaseArray is very useful
+    //     $scope.selectedSong = $scope.songs.$getRecord($scope.songId);
+    //     console.log("$scope.selectedSong", $scope.selectedSong);
+    //   })
+    //   .catch(function(error) {
+    //     console.log("Error:", error);
+    //   });
 
-          $scope.filterSong = function(song) {
-    // console.log(song.$id);
-    console.log("inside filterSong");
-    
-  };
+          // $scope.filterSong = function(song) {
+          // function filterSong() {
+    // $scope.filterSong = function(song) {
+    //   console.log("inside filterSong");
+    //   console.log(song);
+    // };
 
-  
  }]
 );
